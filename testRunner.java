53a83b3a5b5cf167c1343e8466bade522399bb42
chakri <chakradharchakri1998@gmail.com>
2021-12-09 11:57:07 +0530

    bb

---
+++ testRunner.java
@@ -0,0 +1,11 @@
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+
+public class testRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WsdlProject project = new WsdlProject();
+
+	}
+
+}
